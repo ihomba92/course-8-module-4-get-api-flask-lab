13ee26e158f3f7051ed9ee65e686e0716d29f7ec
ihomba92 <ihombakibandidavid@gmail.com>
2026-06-27 14:26:08 +0300

    feat: added routes and methods

--- app.py
+++ app.py
@@ -7,19 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    # TODO: Return a welcome message
+    return jsonify({"Message": "Welcome to our HomePage", "resource_endpoint": "/products"}), 200
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    # TODO: Return all products or filter by ?category=
+    category = request.args.get("category")
+    if category:
+        filtered = [item for item in products if item["category"] == category]
+        return jsonify (filtered), 200
+    return jsonify(products), 200
+
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
-@app.route("/products/<int:id>")
+@app.route("/products/<int:id>", methods=["GET"])
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    # TODO: Return product by ID or 404
+    product = next((p for p in products if p["id"] == id), None)
+    return jsonify(product) if product else(jsonify({"message": "Product not found"}), 404)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5555)
+ 
